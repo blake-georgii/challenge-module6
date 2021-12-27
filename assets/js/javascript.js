40438a814c126cf6fdb77156d2c039225a2dc0c8
Blake Georgii <blakegeorgii@gmail.com>
2021-12-27 14:00:51 -0800

    realized I was being an idiot and just have to work through the hellscape that are promises

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,49 +1,42 @@
-function init() {
-    cityData = fetchCity();
-    weatherData = fetchWeather(cityData);
-
-    console.log(cityData);
-    console.log(weatherData);
-};
-
-async function fetchCity() {
+function fetchCity() {
     let key = 'ca321dd665a915445a40608ae28b8292';
     let limit = 1;
     let cityName = document.getElementById('city-name');
 
     let url = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=${limit}&appid=${key}`;
 
-    cityData = await fetch(url)
-        .then((resp) => {
-            if (!resp.ok) throw new Error(resp.statusText);
-            return resp.json();
+    fetch(url)
+        .then((res) => {
+            if (!res.ok) throw new Error(res.statusText);
+            return res.json();
         })
-        .then((data) => {
-            app.showWeather(data);
+        .then((cityData) => {
+            fetchWeather(cityData);
         })
         .catch(console.err);
-
-    return cityData
 };
 
-async function fetchWeather(cityData) {
+function fetchWeather(cityData) {
     let lat = cityData.lat;
     let lon = cityData.lon;
-    let key = '06cc7efd0e5386068ec3c390bcfd0183';
+    let key = 'ca321dd665a915445a40608ae28b8292';
     let lang = 'en';
     let units = 'imperial';
 
     let url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
     //fetch the weather
-    weatherData = await fetch(url)
-        .then((resp) => {
-            if (!resp.ok) throw new Error(resp.statusText);
-            return resp.json();
-        })
-        .then((data) => {
-            app.showWeather(data);
+    fetch(url)
+    .then((res) => {
+        if (!res.ok) throw new Error(res.statusText);
+        return res.json();
+    })
+        .then((weatherData) => {
+            loadPage(weatherData);
         })
         .catch(console.err);
-
-    return weatherData;
 };
+
+function loadPage(weatherData){
+    console.log(weatherData);
+};
+
