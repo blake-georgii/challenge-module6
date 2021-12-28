2a896668031e4e9c836ea80717902c1a4ea112bf
Blake Georgii <blakegeorgii@gmail.com>
2021-12-27 20:59:44 -0800

    created main weather display

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -29,19 +29,36 @@ function fetchWeather(cityData) {
     let url = `http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
     //fetch the weather
     fetch(url)
-    .then((res) => {
-        if (!res.ok) throw new Error(res.statusText);
-        return res.json();
-    })
+        .then((res) => {
+            if (!res.ok) throw new Error(res.statusText);
+            return res.json();
+        })
         .then((weatherData) => {
             loadPage(weatherData);
         })
         .catch(console.err);
 };
 
-function loadPage(weatherData){
+function loadPage(weatherData) {
     console.log(weatherData);
+    let day = new Date;
+    loadDisplay(weatherData.current, day);
 };
+
+
+function loadDisplay(currentWeather, day) {
+    let display = document.getElementById('current-weather');
+
+    display.innerHTML = `
+    <h2>${form.elements['city-name'].value} (${day.getMonth()}/${day.getDate()}/${day.getFullYear()})</h2>
+    <p>Temp: ${currentWeather.temp}°F</p>
+    <p>Wind: ${currentWeather.wind_speed}MPH</p>
+    <p>Humidity: ${currentWeather.humidity}%</p>
+    <p>UV Index: ${currentWeather.uvi}</p>
+    `
+
+};
+
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
